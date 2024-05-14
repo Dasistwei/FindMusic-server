1d395b5479c0638054f4eaa5b07fa7f06ffe22b3
wei <wei02919@gmail.com>
2024-05-14 21:37:08 +0800

    fix image.js error

--- service/image.js
+++ service/image.js
@@ -1,6 +1,6 @@
 const multer = require('multer');
 const path = require('path');
-
+const appError = require('../service/appError');
 const upload = multer({
   limits: {
     fileSize: 2 * 1024 * 1024,
@@ -8,7 +8,7 @@ const upload = multer({
   fileFilter(req, file, cb) {
     const ext = path.extname(file.originalname).toLowerCase();
     if (ext !== '.jpg' && ext !== '.png' && ext !== '.jpeg') {
-      return cb(new Error('檔案格式錯誤，僅限上傳 jpg、jpeg 與 png 格式。'));
+      return cb(appError(400, '檔案格式錯誤，僅限上傳 jpg、jpeg 與 png 格式。'));
     }
     cb(null, true);
   },
